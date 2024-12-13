63ae24687f6da171d89ceed896320bde009b7312
Justin Lee <justin.lee@alto.com>
2024-12-13 00:05:32 -0800

    feat: add rules for autopair

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -7,9 +7,13 @@ return {
 	-- Optional dependency
 	dependencies = { "hrsh7th/nvim-cmp" },
 	init = function()
+		local npairs = require("nvim-autopairs")
+		local Rule = require("nvim-autopairs.rule")
 		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 		local cmp = require("cmp")
 		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		npairs.add_rules(require("nvim-autopairs.rules.endwise-ruby"))
+		npairs.add_rule(Rule("|", "|", "ruby"))
 	end,
 	opts = {},
 }
