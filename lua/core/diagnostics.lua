04a372993d11101f56eeb5aa707b7fe169efd45c
Justin Lee <justin.lee@verkada.com>
2025-12-04 14:15:37 -0800

    add diagnostics

---
+++ lua/core/diagnostics.lua
@@ -0,0 +1,51 @@
+-- [[ Configure Diagnostics ]]
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+
+-- Diagnostic configuration
+vim.diagnostic.config({
+	virtual_text = {
+		prefix = "●",
+	},
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "󰅚",
+			[vim.diagnostic.severity.WARN] = "󰀪",
+			[vim.diagnostic.severity.HINT] = "󰌶",
+			[vim.diagnostic.severity.INFO] = "󰋽",
+		},
+		texthl = {
+			[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+			[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+			[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+			[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+		},
+	},
+	update_in_insert = false,
+	underline = true,
+	severity_sort = true,
+	float = {
+		focusable = false,
+		style = "minimal",
+		border = "rounded",
+		source = "always",
+		header = "",
+		prefix = "",
+	},
+})
+
+-- Show diagnostics on cursor hold (float window)
+vim.api.nvim_create_autocmd("CursorHold", {
+	buffer = 0,
+	callback = function()
+		local opts = {
+			focusable = false,
+			close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+			border = "rounded",
+			source = "always",
+			prefix = " ",
+			scope = "cursor",
+		}
+		vim.diagnostic.open_float(nil, opts)
+	end,
+})
+
