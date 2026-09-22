f82ca6af6813d84d23d232f1df396291281f62a2
Justin Lee <justin.lee@verkada.com>
2026-09-22 14:36:08 -0700

    fix error

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -27,7 +27,6 @@ require("lazy").setup({
 		{ import = "plugins" },
 		{ import = "plugins.lsp" },
 		{ import = "plugins.colortheme" },
-		{ import = "plugins.testing" },
 	},
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
